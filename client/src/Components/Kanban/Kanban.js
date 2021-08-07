16327e4a044a104a862ede535c588c5cf1652a2c
Ibtid <ibtid011@example.com>
2021-08-07 22:44:08 +0600

    retrieving the task from the kanban

--- client/src/Components/Kanban/Kanban.js
+++ client/src/Components/Kanban/Kanban.js
@@ -7,6 +7,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import { deepPurple } from '@material-ui/core/colors';
 import { useParams } from 'react-router-dom';
 import { getSprintDetails } from '../../API/sprint';
+import Spinkit from '../../Modals/Spinkit/Spinkit';
+import ResponseModal from '../../Modals/ResponseModal/ResponseModal';
 
 import './Kanban.css';
 
@@ -20,85 +22,96 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const itemsFromBackend = [
-  { id: uuidv4(), content: 'First task' },
-  { id: uuidv4(), content: 'Second task' },
-  { id: uuidv4(), content: 'Third task' },
-  { id: uuidv4(), content: 'Fourth task' },
-  { id: uuidv4(), content: 'Fifth task' },
-  { id: uuidv4(), content: 'Last task' },
-];
-
-const columnsFromBackend = {
-  [uuidv4()]: {
-    name: 'Pending',
-    items: itemsFromBackend,
-  },
-  [uuidv4()]: {
-    name: 'Ongoing',
-    items: [],
-  },
-  [uuidv4()]: {
-    name: 'Completed',
-    items: [],
-  },
-};
-
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) return;
-  const { source, destination } = result;
-
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
-    const [removed] = sourceItems.splice(source.index, 1);
-    destItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems,
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems,
-      },
-    });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems,
-      },
-    });
-  }
-};
-
 const Kanban = () => {
   const classes = useStyles();
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
 
-  const [columns, setColumns] = useState(columnsFromBackend);
+  const [columns, setColumns] = useState([]);
+
+  const onDragEnd = (result, columns, setColumns) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems,
+        },
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems,
+        },
+      });
+    }
+  };
 
   const { sprintId } = useParams();
 
   useEffect(() => {
+    setLoading(true);
     let body = {
       sprintId,
     };
     getSprintDetails(body).then((response) => {
       console.log(response);
+      if (response.success) {
+        setColumns({
+          [uuidv4()]: {
+            name: 'Pending',
+            items: response.sprint.pending,
+          },
+          [uuidv4()]: {
+            name: 'Ongoing',
+            items: response.sprint.ongoing,
+          },
+          [uuidv4()]: {
+            name: 'Completed',
+            items: response.sprint.completed,
+          },
+        });
+        setLoading(false);
+      } else {
+        setMessage(response.message);
+        setOpen(true);
+        setLoading(false);
+      }
     });
   }, []);
 
   return (
     <div className='kanban'>
+      {loading && <Spinkit />}
+
+      {open && (
+        <ResponseModal
+          message={message}
+          setOpen={() => {
+            setOpen(false);
+          }}
+        />
+      )}
       <DragDropContext
         onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
         {Object.entries(columns).map(([columnId, column], index) => {
@@ -115,7 +128,7 @@ const Kanban = () => {
                           ref={provided.innerRef}
                           style={{
                             background: snapshot.isDraggingOver
-                              ? '#525252'
+                              ? '#3a3b3c'
                               : '#252525',
                             width: '100%',
                             minHeight: '100%',
@@ -123,8 +136,8 @@ const Kanban = () => {
                           {column.items.map((item, index) => {
                             return (
                               <Draggable
-                                key={item.id}
-                                draggableId={item.id}
+                                key={item._id}
+                                draggableId={item._id}
                                 index={index}>
                                 {(provided, snapshot) => {
                                   return (
@@ -138,19 +151,23 @@ const Kanban = () => {
                                         margin: '0 0 1vh 0',
                                         minHeight: '1vh',
                                         backgroundColor: snapshot.isDragging
-                                          ? '#FF8C42'
+                                          ? '#525252'
                                           : '#252525',
                                         color: 'white',
                                         ...provided.draggableProps.style,
                                       }}>
-                                      <div className='task__div'>
-                                        <div className='task__story'>
-                                          <div className='task__color'></div>
-                                          {item.content}
+                                      <div className='task '>
+                                        <div className='bullet noMargin'>
+                                          <div className={item.color}></div>
                                         </div>
-                                        <Avatar className={classes.purple}>
-                                          N
-                                        </Avatar>
+                                        <div className={`task__text  `}>
+                                          {item.story}
+                                        </div>
+
+                                        <Avatar
+                                          className={classes.purple}
+                                          src={`http://localhost:5000/${item.assignedTo.image}`}
+                                        />
                                       </div>
                                     </div>
                                   );
